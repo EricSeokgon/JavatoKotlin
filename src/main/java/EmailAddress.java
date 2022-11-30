de2c0fddec2b6eeec3acbd8f77e0ae45d0a8aa2b
hadesyi <leesk55@gmail.com>
2022-11-30 15:38:59 +0900

    간단한 값 타입 EmailAddress 클래스 Java to kotlin 변경

---
+++ src/main/java/EmailAddress.java
@@ -0,0 +1,46 @@
+public class EmailAddress {
+    private final String localPart;
+    private final String domain;
+
+    public static EmailAddress parse(String value) {
+        var atIndex = value.lastIndexof('@');
+        if (atIndex < 1 || atIndex == value.legth() - 1) {
+            throw new IllegalArgumentExceptrion(
+                    "EmailAddress must be two parts separated by @"
+            );
+            return new EmailAddress(value.substring(0, atIndex), value.substring(atIndex + 1));
+        }
+    }
+
+    public EmailAddress(String localPart, String domain) {
+        this.localPart = localPart;
+        this.domain = domain;
+    }
+
+    public String getLocalPart() {
+        return localPart;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EmailAddress that = (EmailAddress) o;
+        return localPart.equals(that.localPart) && domain.equals(that.domain);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(localPart, domain);
+    }
+
+    @Override
+    public String toString() {
+        return localPart + "@" + domain
+    }
+
+}
---
+++ src/main/kotlin/EmailAddress.kt
@@ -0,0 +1,30 @@
+import java.util.Objects
+
+class EmailAddress(val localPart: String, val domain: String) {
+    override fun equals(o: Any?): Boolean {
+        if (this == o) return true
+        if (o == null || javaClass != o.javaClass) return false
+        val that = o as EmailAddress
+        return localPart == that.localPart && domain == that.domain
+    }
+
+    override fun hashCode(): Int {
+        return Objects.hash(localPart, domain)
+    }
+
+    override fun toString(): String {
+        return "$localPart@$domain"
+    }
+
+    companion object {
+        @JvmStatic
+        fun parse(value: String): EmailAddress {
+            val atIndex = value.lastIndexOf('@')
+            require(!(atIndex < 1 || atIndex == value.length - 1)) {
+                "EmailAddress must be two parts separated by @"
+            }
+            return EmailAddress(value.substring(0, atIndex), value.substring(atIndex + 1))
+
+        }
+    }
+}

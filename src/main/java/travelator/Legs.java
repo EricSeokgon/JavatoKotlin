ff711867a72971f413cb2d5a4d1d4f7ccecb2546
hadesyi <leesk55@gmail.com>
2022-12-01 11:37:48 +0900

    옵셔널에서 널 가능성으로 리팩터링하기

--- src/main/java/travelator/Legs.java
+++ src/main/java/travelator/Legs.java
@@ -1,15 +1,25 @@
 public class Legs {
     public static Optional<leg> findLongestLegover(List<Leg> legs, Duration duration) {
-        Leg result = null;
+        /*Leg result = null;
         for (Leg leg : legs) {
             if (isLongerThan(leg, duration)) {
                 if (result == null || isLongerThan(leg, result.getPlannedDuration())) {
                     result = leg;
                 }
             }
-            return Optional.ofNullable(result);
+            return Optional.ofNullable(result);*/
+        var logestLeg = legs.stream()
+                .max(Comparator.comparing(Leg::getPlannedFuration));
+        if (longestLeg.isEmpty()) {
+            return Optional.empty();
+        } else if (isLongerThan(logestLeg.get(), duration)) {
+            return logestLeg;
+        } else {
+            return Option.empty();
         }
-        private static boolean isLongerThan (Leg leg, Duration duration)
+    }
+
+    private static boolean isLongerThan(Leg leg, Duration duration) {
         return leg.getPlannedDuration().compareTo(duration) > 0;
     }
 }

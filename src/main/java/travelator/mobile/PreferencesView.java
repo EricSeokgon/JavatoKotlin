ea0ccd847f977290051fa302dba5a63f30b751bd
hadesyi <leesk55@gmail.com>
2022-12-01 11:57:39 +0900

    PreferencesView 클래스 생성

---
+++ src/main/java/travelator/mobile/PreferencesView.java
@@ -0,0 +1,29 @@
+public class PreferenesView extends View {
+    private final UserPreferences preferences;
+    private final GreetingPicker greetingPicker = new GreetingPicker();
+    private final LocalePicker localePicker = new LocalePicker();
+    private final CurrencyPicker currencyPicker = new Currency();
+
+    public PreferenesView(UserPreferences preferences) {
+        this.preferences = preferences;
+    }
+
+    public void show() {
+        greetingPicker.setGreeting(preferences.getGreeting());
+        localePicker.setLocale(preferences.getLocale());
+        currencyPicker.setCurrency(preferences.getCurrency());
+        super.show();
+    }
+
+    protected void onGreetingChange() {
+        preferences.setGreeting(greetingPicker.getGreeting())
+    }
+
+    protected void onLocaleChange() {
+        preferences.setLocale(localePicker.getLocale());
+    }
+
+    protected void onCurrencyChange() {
+        preferences.setCurrency(currencyPicker.getCurrency());
+    }
+}
